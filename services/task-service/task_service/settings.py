21b05e351cc9418085bf3573b0b70ccbbab4e0b8
Shingai Dzinotyiweyi <shingai.dzinotyiweyi@gmail.com>
2026-02-10 02:32:04 +0200

    Update settings.py

--- services/task-service/task_service/settings.py
+++ services/task-service/task_service/settings.py
@@ -81,23 +81,23 @@ WSGI_APPLICATION = 'task_service.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': os.getenv('DB_NAME', 'task_management'),
-#         'USER': os.getenv('DB_USER', 'taskuser'),
-#         'PASSWORD': os.getenv('DB_PASSWORD', 'taskpass123'),
-#         'HOST': os.getenv('DB_HOST', 'postgres'),
-#         'PORT': os.getenv('DB_PORT', '5432'),
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME', 'task_management'),
+        'USER': os.getenv('DB_USER', 'taskuser'),
+        'PASSWORD': os.getenv('DB_PASSWORD', 'taskpass123'),
+        'HOST': os.getenv('DB_HOST', 'postgres'),
+        'PORT': os.getenv('DB_PORT', '5432'),
+    }
+}
 
 
 # Password validation
@@ -284,5 +284,6 @@ STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
